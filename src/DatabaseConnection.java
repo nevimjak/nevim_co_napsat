13038d93a702dd1e299345c2c2cf3fc42d1be1e2
nevimjak01 <belka.robert02@gmail.com>
2023-03-15 17:35:06 +0100

    nevim co dělám, fakt supr :)

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -4,9 +4,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/chat";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://vps.hintik.cz/RobertChat";
+    private static final String USERNAME = "nevimjak";
+    private static final String PASSWORD = "gjfWJ]@eYtk72yG[";
 
     private static Connection connection;
 
